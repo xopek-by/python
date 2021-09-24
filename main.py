5948a43f0ca2d124d2d7644f4b84305fd3ec443e
xopek-by <79866323+xopek-by@users.noreply.github.com>
2021-09-24 16:32:37 +0300

    Lenght of name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # variables
 name = input("What is your name? \n")
 lenght = len(name)
-# printing code
+# printing code, with lenght
 print ("Hello, " + str(name) + "!\n" + "Your name lenght is " + str(lenght))
