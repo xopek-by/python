3fa74dc193b6e3e197a40cad6e074f5461ffc56d
Max Buz <buz.maxim@gmail.com>
2021-09-28 15:31:29 +0300

    Changed numbers after decimal in rounding

--- Day_2/D2_BMI_calc.py
+++ Day_2/D2_BMI_calc.py
@@ -7,8 +7,8 @@ BMI = (float(weight) / float(height) ** 2)
 
 # Cheking results and showing warning message
 if BMI <= 18.5:
-    print("Your BMI is " + str(round(BMI, 2)) + " its too low, you need start to eat more")
+    print("Your BMI is " + str(round(BMI, 1)) + " its too low, you need start to eat more")
 elif BMI >= 24.9:
-    print("Your BMI is " + str(round(BMI, 2)) + " its too high, you need to control your diet")
+    print("Your BMI is " + str(round(BMI, 1)) + " its too high, you need to control your diet")
 else:
-    print("Your BMI is " + str(round(BMI, 2)) + " its super, you are awesome")
+    print("Your BMI is " + str(round(BMI, 1)) + " its super, you are awesome")
