5d9418fed2aa0d06d52ecfa5b24668e57aa83a89
Max Buz <buz.maxim@gmail.com>
2021-09-28 16:50:58 +0300

    Change variabels names

--- Day_2/D2_bill_calc.py
+++ Day_2/D2_bill_calc.py
@@ -13,5 +13,5 @@ peoples_calc = int(peoples)
 bill_with_tip = float(bill) * (float(percent) / 100 + 1)
 
 # Calculating the final sum
-sum = round(bill_with_tip / peoples_calc, 2)
-print(f"So, you spend {bill} dollars with {int(peoples_calc-1)} friends, and decide to pay {percent}% tips. Each people should pay {sum} dollars")
+bill_per_person = round(bill_with_tip / peoples_calc, 2)
+print(f"So, you spend {bill} dollars with {int(peoples_calc-1)} friends, and decide to pay {percent}% tips. Each people should pay {bill_per_person} dollars")
