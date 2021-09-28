1b9385b41305844b76bfc376d415f951c9a56ba3
Max Buz <buz.maxim@gmail.com>
2021-09-28 16:39:22 +0300

    Minor comments changes

--- Day_2/D2_bill_calc.py
+++ Day_2/D2_bill_calc.py
@@ -1,5 +1,8 @@
 # Welcome to the party calculator. He will calculate how much each of the participants in the party should pay, including a tip to the waiter.
 
+# Printing greetings
+print("Welcome to the tip calculator")
+
 # Reading total bill, number of peoples and tips persentage
 bill = input("What was total bill?\n$")
 peoples = input("How many people to split the bill\n")
