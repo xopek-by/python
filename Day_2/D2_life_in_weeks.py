a57265f85e6db543a355f4b120467601c96b99ad
Max Buz <buz.maxim@gmail.com>
2021-09-28 15:54:36 +0300

    Life left calculator

---
+++ Day_2/D2_life_in_weeks.py
@@ -0,0 +1,15 @@
+# This programm is calculating how many month, weeks and days is left for you, if you will live until 90 years
+
+# Waiting an age input from user
+age = int(input("What is your current age?\n"))
+
+# Calculating years left to 90
+y_left = 90 - age
+
+# Calculating days, weeks and month
+month = y_left * 12
+weeks = y_left * 52
+days  = y_left * 365
+
+# Writing the answer
+print(f"Your have {y_left} years until 90. This is {month} month. Or {weeks} weeks. Or {days} days")
